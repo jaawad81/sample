780d76f4c3b80a33c023a2a6627783e31f519df6
Jawad Ahmed <144881314+jaawad81@users.noreply.github.com>
2024-06-12 13:31:35 +0000

    typeConversion

---
+++ sample/01_type_conversion.js
@@ -0,0 +1,22 @@
+let value="9283"
+let str="Jawad"
+let emptyString=""
+
+//console.log(typeof (value));
+//console.log(typeof str);
+
+let strToNumber=Number(value)
+//console.log(typeof strToNumber);
+
+let number=1
+let numberToBoolean= Boolean(number)
+// console.log(typeof numberToBoolean);
+// console.log(numberToBoolean);
+
+
+let a="77u"
+let stringToNumber= Number(a)
+console.log(stringToNumber);
+
+console.log(typeof stringToNumber);
+
--- sample/index.js
+++ sample/index.js
@@ -1,1 +1,12 @@
-console.log("JAvascript");
+let a=20
+const b=301
+var  c=39210
+console.table([a,c])
+if(b>300){
+   
+   var c=21
+}else{
+    
+}
+
+console.table([a,c])
