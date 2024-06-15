2c86c051f711b19b0e92c69158e298e8ca7925cb
Jawad Ahmed <144881314+jaawad81@users.noreply.github.com>
2024-06-15 11:56:18 +0000

    arrays

---
+++ arrays/01_arrays.js
@@ -0,0 +1,21 @@
+const arr=["Mango","Orange","Apple"]
+
+
+const sli=arr.slice(1,2);
+console.log(sli);
+console.log(arr);
+console.log("---------");
+
+const spli=arr.splice(1,2);
+// console.log(spli);
+// console.log(arr);
+
+
+
+const arr1=[2,4,5,6,3]
+const arr2=["Jammu","Kashmir","Bhimber"]
+
+const arr3=arr1.concat(arr2);
+const arr4= [...arr1,...arr2];
+console.log(arr3);
+console.log(arr4);
